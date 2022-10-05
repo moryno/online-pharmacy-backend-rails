963e026f61d70434387af7f4cac315eded612fd0
Maurice <mauricenganga41@gmail.com>
2022-10-05 08:28:01 +0300

    Add create action in users controller to register a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+    def create
+        @user = User.create(user_params);
+
+        if @user.valid?
+            token = encode_token({user_id: @user.id});
+            render json: {user: @user, jwt: token}, status: :created;
+        else
+            render json: {error: "User was not created"}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
 end
