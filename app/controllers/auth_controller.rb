134d86be3ec7160d9051b5e4d946e97d1665ae0d
Maurice <mauricenganga41@gmail.com>
2022-10-05 09:14:24 +0300

    Add login action on auth controllers and login route for users to login in

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,5 @@
 class AuthController < ApplicationController
-    def create
+    def register
         @user = User.create(user_register_params);
 
         if @user.valid?
@@ -10,10 +10,25 @@ class AuthController < ApplicationController
         end
     end
 
+    def login
+        @user = User.find_by(username: user_login_params[:username])
+
+        if @user && @user.authenticate(user_login_params[:password])
+            token = encode_token({user_id: @user.id});
+            render json: {user: @user, token: token}, status: :ok;
+        else
+            render json: {error: "Invalid username or password"}, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def user_register_params
         params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+    end
+
+    def user_login_params
+        params.require(:user).permit(:username, :password)
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :products
   resources :users, only: [:create]
-  post "/register", to: "auth#create"
-  
+  post "/register", to: "auth#register"
+  post "/login", to: "auth#login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
