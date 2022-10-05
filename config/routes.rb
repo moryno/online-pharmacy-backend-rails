d3a0a7d4cdd4251c221952d5735355987daee5ca
Maurice <mauricenganga41@gmail.com>
2022-10-05 08:30:21 +0300

    Add route to register user

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,8 @@ Rails.application.routes.draw do
   resources :profiles
   resources :order_items
   resources :orders
- 
   resources :products
-
-
-
-
-  
-
-  resources :users
+  resources :users, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
