4901a62d217d163f8d8af2092b49dc5bfa24d7e9
Maurice <mauricenganga41@gmail.com>
2022-10-06 08:02:36 +0300

    Add show action in the products controller to display single product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show]
 
     def create
         if @user.profile.is_admin
@@ -14,9 +15,27 @@ class ProductsController < ApplicationController
         end
     end
 
+    def show
+        product = find_product
+
+        if product
+            render json: product, status: :ok
+        else
+            render_product_not_found_response
+        end
+    end
+
     private
 
     def product_params
         params.permit(:title, :description, :image, :categories, :price, :in_stock)
+    end
+
+    def find_product
+        Product.find_by(id: params[:id])
+    end
+
+    def render_product_not_found_response
+        render json: {error: "Product is not available."}
     end
 end
