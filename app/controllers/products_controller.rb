7e700b922077ddb9ccace57cfc0281d00e7bd4bf
Maurice <mauricenganga41@gmail.com>
2022-10-06 07:52:45 +0300

    Add create action in the products controller to allow admin to create a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+
+    def create
+        if @user.profile.is_admin
+            product = Product.create(product_params)
+           
+            if product.valid?
+                render json: product, status: :created
+            else
+                render json: {error: "Failed to create user"}
+            end
+        else
+            render json: {error: "Only admin is authorized to do that!"}
+        end
+    end
+
+    private
+
+    def product_params
+        params.permit(:title, :description, :image, :categories, :price, :in_stock)
+    end
 end
