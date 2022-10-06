bcce98e5116b0aeb19728459ca0b8a7725f6fb0d
Maurice <mauricenganga41@gmail.com>
2022-10-06 08:07:24 +0300

    Add index action in the products controller to display all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,10 +8,21 @@ class ProductsController < ApplicationController
             if product.valid?
                 render json: product, status: :created
             else
-                render json: {error: "Failed to create user"}
+                render json: {error: "Failed to create product"}
             end
         else
             render json: {error: "Only admin is authorized to do that!"}
+        end
+    end
+
+    def index
+        query = params[:new]
+        products = query ? Product.all.order(created_at: :desc).limit(5) : Product.all
+
+        if products
+            render json: products, status: :ok
+        else
+            render_product_not_found_response
         end
     end
 
